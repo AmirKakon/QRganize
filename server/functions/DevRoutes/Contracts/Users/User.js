ef30e435a21c0f8a1cbb91aef35262989d5e73fe
amirkakon <amir.kakon@gmail.com>
2025-02-25 08:34:22 +0200

    user contract

---
+++ server/functions/DevRoutes/Contracts/Users/User.js
@@ -0,0 +1,22 @@
+/**
+ * @class User
+ * @description User object
+ */
+class User {
+    /**
+     * Creates a User instance.
+     * @param {string} id - ID of the User
+     * @param {string} name - name for the User
+     * @param {string} email - The user email
+     * @param {string} phone - the phone number of the user (including country code)
+     */
+    constructor(id, name, email, phone) {
+      this.id = id;
+      this.name = name;
+      this.email = email;
+      this.phone = phone;
+    }
+  }
+  
+  module.exports = User;
+  
