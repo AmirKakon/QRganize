40ea21307440e89d2be623a92586356c33abfe77
amirkakon <amir.kakon@gmail.com>
2025-03-22 23:24:57 +0200

    items page

--- src/pages/ItemPage/index.js
+++ src/pages/ItemPage/index.js
@@ -1,24 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 import { useParams } from "react-router-dom";
+import { searchForBarcode } from "../../utilities/api";
 import Loading from "../../components/Loading";
-
 
 const ItemPage = ({ isSmallScreen }) => {
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
+  const [item, setItem] = useState(null);
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "auto" });
-    setLoading(false);
-  }, []);
+
+    searchForBarcode(id)
+      .then((res) => {
+        console.log(res);
+        setItem(res[0]);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [id]);
+
+  const getImageSrc = (image) => {
+    // Check if the image is a valid base64 string
+    if (image && !image.startsWith("data:image")) {
+      return `data:iamge/png;base64,${image}`;
+    }
+    return image;
+  };
 
   return loading ? (
     <Loading />
   ) : (
-    <Box flex={1} spacing={1} sx={{backgroundColor: "#e2e2e2"}}>
+    <Box flex={1} spacing={1} sx={{ backgroundColor: "#e2e2e2" }}>
       <h3>Item Page</h3>
-      <p>the id is: {id}</p>
+      <p>The ID is: {id}</p>
+      <p>The Item ID is: {item?.id}</p>
+      <p>The Item Name is: {item?.name}</p>
+
+      {item?.image && (
+        <Box sx={{ marginTop: 2 }}>
+          <img
+            src={getImageSrc(item?.image)}
+            alt={item?.name}
+            style={{ width: "300px", borderRadius: "8px" }}
+          />
+        </Box>
+      )}
     </Box>
   );
 };
--- src/utilities/api.js
+++ src/utilities/api.js
@@ -1,1 +1,16 @@
 const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
+
+export const searchForBarcode = async (id) => {
+  const response = await fetch(`${apiBaseUrl}/api/items/searchBarcode/${id}`, {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("accessToken"),
+      "Content-Type": "application/json",
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status}`);
+  }
+  const res = await response.json();
+  return res.data.data;
+};
