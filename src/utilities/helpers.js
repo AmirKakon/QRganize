a76678066f0bae6c58c7d7903916652aceb49e87
amirkakon <amir.kakon@gmail.com>
2025-03-26 21:50:30 +0200

    add item

--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -4,3 +4,10 @@ export const getImageSrc = (image) => {
       return `data:image/png;base64,${image}`;
     return image;
   };
+
+export const generateRandomId = (length = 10) => {
+  const min = Math.pow(10, length - 1);
+  const max = Math.pow(10, length) - 1;
+  return Math.floor(min + Math.random() * (max - min + 1)).toString();
+};
+
